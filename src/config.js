ef39ff98e4b38b57180758a404c35d34a09ee808
Katie Hansen <khansen0415@gmail.com>
2019-11-25 18:17:40 -0500

    add api

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://dev-tracks-api.herokuapp.com/',
+    API_ENDPOINT: 'https://dev-tracks-api.herokuapp.com/api',
     TOKEN_KEY: 'dev-tracks-client-auth-token',
   }
