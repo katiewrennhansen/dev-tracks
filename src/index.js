a8aec0b24db7022e156253b8ab9855148fdbd36c
Katie Hansen <khansen0415@gmail.com>
2019-11-27 09:28:34 -0500

    switch api endpoint back

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
-import { ResourceProvider } from './contexts/ResourceContext';
 
 ReactDOM.render(
     <BrowserRouter>
-        <ResourceProvider>
             <App />
-        </ResourceProvider>
-    </BrowserRouter>    ,
+    </BrowserRouter>,
     document.getElementById('root')
 );
 
